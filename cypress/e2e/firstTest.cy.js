7c831e150ac38e1f84b6d078dbe5642bcb82db18
Gaddi4sid <garnaiksiddharth3@gmail.com>
2024-03-20 14:01:22 +0530

    test update

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,5 +1,13 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://www.3pointsfootball.com')
+    cy.visit('https://www.bing.com/images/create?FORM=GENILP')
+    cy.get("[data-sm='Surprise Me']").click();
+    cy.get("[title='Sign in with a personal account']").click();
+    cy.get("[name='loginfmt']").type("9765539236")
+    cy.get("[name='passwd']").type("Kanha@2306")
+    cy.get("#acceptButton").click();
+    cy.get(".b_searchbox gi_sb").type("A bird with an arsenal Tshirt")
+    cy.get("[name='Create']").click();
+    cy.wait(10000)
   })
 })
